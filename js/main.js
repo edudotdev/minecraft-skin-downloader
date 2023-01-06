c3a5237b62946061ff4200017f076b382edb7ce8
edudotdev <eduardo.er626@gmail.com>
2023-01-06 02:19:59 -0700

    feat: reworked app

--- js/main.js
+++ js/main.js
@@ -1,63 +1,65 @@
 import skinView from "./skinview.js";
+import { capeNames } from "./capes.js";
 
 const btnSearch = document.querySelector('#search');
 const nameUser = document.querySelector('#nameUser');
-const inputUUID = document.querySelector('#uuid');
-const inputCommandHead = document.querySelector('#CommandHead');
-const head = document.querySelector('#head');
+const btnUUID = document.querySelector('#uuid');
+const skinRender = document.querySelector('#skin');
+const headRender = document.querySelector('#head');
+const avatarRender = document.querySelector('#avatar');
 const playerName = document.querySelector('#playerName');
-const nameHistory = document.querySelector('#nameHistory');
-const copy = document.querySelectorAll('#copy');
 const btnSkin = document.querySelector('#btnSkin');
+const btnApply = document.querySelector('#btnApply');
+const url = document.querySelector('#url')
+const tooltipCopy = document.querySelectorAll('#tooltipCopy')
+const wrapper_cape = document.querySelector('#wrapper_cape')
 
-// initial skinView
-skinView('../src/images/steve.png')
+// initial skin
+skinView('http://textures.minecraft.net/texture/4ceb7ffc87d2859c7eea8c7aec7bf0265fd866876c1c820ed22cb38dafedee55')
 
-btnSearch.addEventListener('click', (e) => {
-  e.preventDefault()
+// set current url
+url.innerHTML = window.location.href
 
-  if(nameUser.value.length <= 2)  return  
+// data to html
+const data_DOM = (playerInfo) => {
 
-  fetch(`https://playerdb.co/api/player/minecraft/${nameUser.value}`)
-  .then(response => response.json())
-  .then(data => {
-    data_DOM(data.data.player);
-  })
-  .catch(() => {
-    errorFetch()
-  })
-})
+  if(playerInfo === undefined) return
 
-copy.forEach(btn => { btn.addEventListener('click', e => copyToClipboard(e))})
+  const {profileId, profileName, textures} = playerInfo;
 
-// DOM data
-const data_DOM = (data) => {
-  const {raw_id, username, meta} = data;
+  const skin = textures.SKIN.url
+  const hash = skin.split('/')[skin.split('/').length - 1]
 
-  inputUUID.value = raw_id; 
-  inputCommandHead.value = `/give @p minecraft:player_head{SkullOwner:"${username}"}`;
-  head.src = `https://crafatar.com/renders/head/${raw_id}`;
-  playerName.innerText = username;
-  rendersNames(meta.name_history);
-  const skin = `https://crafatar.com/skins/${raw_id}`
-  btnSkin.href = skin;
+  if(textures.CAPE?.url !== undefined) {
+    const cape = textures.CAPE.url
+    const hashCape = cape.split('/')[cape.split('/').length - 1]
+    const nameCapePlayer = capeNames[hashCape]
 
+    wrapper_cape.innerHTML = `
+      <div class="min-w-[78px] max-w-[78px] h-[125px] rounded-md overflow-hidden">
+        <img class="min-w-[500px] relative -top-2 -left-2 cape-image" src="${textures.CAPE.url}" alt="" >
+      </div>
+      <h2 class="text-gray-200 text-xl font-semibold mb-2 text-center md:text-left">${nameCapePlayer} Cape</h2>
+    `
+  } else {
+    wrapper_cape.innerHTML = `
+      <h2 class="text-gray-400 text-2xl font-semibold text-center md:text-left">No Cape</h2>
+    `
+  }
+
+  btnUUID.innerText = addDashesToUUID(profileId)
+  playerName.innerText = profileName;
+  avatarRender.src = `https://mc-heads.net/avatar/${hash}/120`;
+  headRender.src = `https://mc-heads.net/head/${hash}/200`;
+  skinRender.src = `https://mc-heads.net/body/${hash}/200`;
+  btnApply.href = `https://www.minecraft.net/profile/skin/remote?url=${skin}`
+  
+  btnSkin.dataset.url = skin;
   skinView(skin)
 }
 
-const rendersNames = (names) => {
-  while (nameHistory.firstChild) {
-    nameHistory.removeChild(nameHistory.firstChild);
-  }
-  names.map(name => {
-    const li = document.createElement('li');
-    li.innerText = name.name;
-    document.querySelector('#nameHistory').appendChild(li);
-  })
-}
-
 const copyToClipboard = (e) => {
-  const text = e.target.previousElementSibling.value;
+  const text = e.target.innerText;
   const input = document.createElement('input');
   input.value = text;
   document.body.appendChild(input);
@@ -66,16 +68,89 @@ const copyToClipboard = (e) => {
   document.body.removeChild(input);
 }
 
+// download image
+const downloadImage = async (e) => {
+  const imageSrc = e.target.dataset.url
+  const image = await fetch(imageSrc)
+  const imageBlog = await image.blob()
+  const imageURL = URL.createObjectURL(imageBlog)
+
+  const link = document.createElement('a')
+  link.href = imageURL
+  link.download = `${playerName.innerText}-minecraft-skin`
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
+const getID = async (username) => {
+  let id = await fetch(`https://playerdb.co/api/player/minecraft/${username}`)
+    .then(response => response.json())
+    .then(data => {
+      return data.data.player.raw_id
+    })
+    .catch(() => {
+      errorFetch()
+    })
+  return id
+}
+
+// player not found
 const errorFetch = () => {
-  const wrapperMain = document.querySelector('body main');
+  const wrapperInput = document.querySelector('#wrapper-input');
   const error = document.createElement('p');
 
-  error.innerText = 'Player not found, search other player';
-  error.classList.add('order-2', 'text-center', 'py-3', 'text-xl', 'font-bold', 'mc-error', 'bg-red-400');
+  error.innerText = 'Player not found';
+  error.classList.add('order-first', 'absolute', 'w-full','text-center', 'py-3', 'text-xl', 'font-bold', 'text-amber-800','bg-amber-400', '-bottom-16', 'rounded-md');
 
-  wrapperMain.appendChild(error);
+  wrapperInput.appendChild(error);
+  
   setTimeout(() => {
-    wrapperMain.removeChild(error);
+    wrapperInput.removeChild(error);
   }
   , 2500);
 }
+
+const addDashesToUUID = (uuid) => {
+  return uuid.slice(0, 8) + '-' + uuid.slice(8, 12) + '-' + uuid.slice(12, 16) + '-' + uuid.slice(16, 20) + '-' + uuid.slice(20);
+}
+
+// events
+btnUUID.addEventListener('click', e => {
+  copyToClipboard(e)
+  tooltipCopy[0].innerText = 'Copied'
+  setTimeout(()=> {
+    tooltipCopy[0].innerText = 'Copy'
+  }, 1000)
+})
+url.addEventListener('click', e => {
+  copyToClipboard(e)
+  tooltipCopy[1].innerText = 'Copied'
+  setTimeout(() => {
+    tooltipCopy[1].innerText = 'Copy'
+  }, 1000)
+})
+btnSkin.addEventListener('click', e => downloadImage(e))
+btnSearch.addEventListener('click', async (e) => {
+  e.preventDefault()
+  if(nameUser.value.length < 2 && nameUser.value.length > 16) return
+  const id = await getID(nameUser.value)
+  fetch(`https://api.minetools.eu/profile/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      data_DOM(data.decoded)
+    })
+})
+
+const initApp = async (username) => {
+  const id = await getID(username)
+  fetch(`https://api.minetools.eu/profile/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      data_DOM(data.decoded)
+    })
+    .catch((error) => {
+    })
+}
+
+initApp('eduardorl')
