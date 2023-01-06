bd8ba2d2f8f7815dc86c99af69bb0f1c11199c02
edudotdev <eduardo.er626@gmail.com>
2023-01-06 02:17:04 -0700

    feat: custom initial position

--- js/skinview.js
+++ js/skinview.js
@@ -2,18 +2,35 @@ export default function skinView (skin) {
 
 let skinViewer = new skinview3d.SkinViewer({
   canvas: document.getElementById("skin_container"),
-  width: 320,
-  height: 600,
+  width: 280,
+  height: 550,
   skin: skin,
 });
 
-skinViewer.fov = 70;
-skinViewer.zoom = .9;
+initializePosition()
+initializeControls()
 
 skinViewer.animations.add(skinview3d.WalkingAnimation);
 
 let control = skinview3d.createOrbitControls(skinViewer);
-control.enableRotate = true;
-control.enableZoom = false;
-control.enablePan = false;
+  control.enableRotate = true;
+  control.enableZoom = false;
+  control.enablePan = false;
+  skinViewer.autoRotate = true;
+
+  function initializeControls() {
+    document.getElementById("animation_pause_resume").addEventListener("click", () => skinViewer.animations.paused = !skinViewer.animations.paused);
+  }
+
+  function initializePosition() {
+    skinViewer.fov = 30;
+    skinViewer.zoom = .9;
+    skinViewer.camera.position.x = -20.5;
+    skinViewer.camera.position.y = 0;
+
+    setTimeout(() => {
+      skinViewer.animations.paused = true;
+    }, 100)
+  }
+
 }
